c2a932d9cb9655106f1eaac463336f904e84e0f3
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-02 21:30:12 +0100

    Display informations about a participant

--- carotene/urls.py
+++ carotene/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'person.views.index', name='index'),
     url(r'^participant$', 'person.views.create_participant', name='participants'),
+    url(r'^participants/(\d+)/$', 'person.views.view_participant', name='view_participant'),
     url(r'^mentor$', 'person.views.create_mentor', name='mentors'),
     url(r'^admin/', include(admin.site.urls)),
 )
-    
