2fe45a84534c4b4d8891707379df9b69a69c263d
Sem Ladjal <sem2173@users.noreply.github.com>
2015-11-29 15:15:49 +0100

    change email

--- person/tests.py
+++ person/tests.py
@@ -9,12 +9,12 @@ class ParticipantTestCase(TestCase):
 
     def test_participant_create_one_person(self):
         fanta = Participant.objects.create(first_name='fanta',
-                last_name= 'carrot',email='semia.ladjal@gmail.com',birth_date='2015-03-09',
+                last_name= 'carrot',email='test.test@gmail.com',birth_date='2015-03-09',
                 phone_number='000000000',english_level='Never',gender='women' )
 
         self.assertEqual(fanta.first_name, 'fanta'),
         (fanta.last_name, 'carrot'),
-        (fanta.email, 'semia.ladjal@gmail.com'),
+        (fanta.email, 'test.test@gmail.com'),
         (fanta.birth_date,'2015-03-09'),
         (fanta.phone_number, '000000000'),
         (fanta.english_level,'Never'),
